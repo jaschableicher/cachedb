90bc5116d6a17033c32c14a49d5ba83891f97619
Jascha Bleicher <jascha@bleicher-dev.de>
2026-09-03 14:26:10 +0200

    CleanUp

--- src/commands/executor.cpp
+++ src/commands/executor.cpp
@@ -1,7 +1,5 @@
 #include "executor.h"
-#include "parser.h"
-#include "registry.h"
-#include "utils.h"
+
 std::string execute_command(Database& db, std::string line){
     Command command = parse_command(line);
     CommandContext context{db};
