9ba6e2d10f0cf77b8af17b90a8a6d89c3faa9d7b
Jascha Bleicher <jascha@bleicher-dev.de>
2026-09-08 15:03:04 +0200

    Add snapshot to config, comment as note

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,9 @@ int main() {
     Database db;
     TCPServer server(db);
     //replay database.aof cache
+    //on a clean exit the last command on database.aof must be SAVE for the datadump
+    //If no SAVE existed, the programm crashed so recover the latest datadump and then from the last SAVE replay the commands
+    //Then if
     Logger::get_instance()->replay_commands(db);
     std::thread server_thread(&TCPServer::run, &server);
 
