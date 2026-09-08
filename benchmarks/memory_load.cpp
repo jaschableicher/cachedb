11ca928194ca613018f9291263147fc7e720f743
Jascha Bleicher <jascha@bleicher-dev.de>
2026-09-08 15:02:35 +0200

    Create files for memory testing

---
+++ benchmarks/memory_load.cpp
@@ -0,0 +1,8 @@
+//TODO: write large amount of data to the server
+// Test at random data
+// BEnchmark all singular commands and overall timing
+// Standard 1000 clients
+// Minimam 1 Mio requests/get/set commands played
+
+//Return req/s and latency
+//Then check where most stuff happens
