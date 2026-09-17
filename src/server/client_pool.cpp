78893d2182419ad8cafbe71996ff6c2e8021bada
Jascha Bleicher <jascha@bleicher-dev.de>
2026-09-17 10:46:38 +0200

    Implement split_keep_newline function and update handle_client_data to process multiple commands in one sending process

--- src/server/client_pool.cpp
+++ src/server/client_pool.cpp
@@ -93,6 +93,32 @@ void ClientWorker::handle_new_clients() {
     }
 }
 
+
+std::vector<std::string> split_keep_newline(std::string_view str) {
+    std::vector<std::string> result;
+    size_t start = 0;
+    
+    while (start < str.size()) {
+        // Find the next newline character
+        size_t pos = str.find('\n', start);
+        
+        if (pos == std::string_view::npos) {
+            // No more newlines, grab the remaining chunk
+            result.emplace_back(str.substr(start));
+            break;
+        }
+        
+        // Extract substring *including* the newline character (+ 1)
+        size_t length = (pos - start) + 1;
+        result.emplace_back(str.substr(start, length));
+        
+        // Move start pointer past the newline
+        start = pos + 1;
+    }
+    
+    return result;
+}
+
 void ClientWorker::handle_client_data(int client_fd){
     char buffer[1024];
     ssize_t bytes_read =  recv(client_fd, buffer, sizeof(buffer) - 1,  0);
@@ -103,19 +129,30 @@ void ClientWorker::handle_client_data(int client_fd){
         close(client_fd);
     }
     else{
-      
-        message_pool_[client_fd].append(buffer, bytes_read);
-        if(buffer[bytes_read-1]!='\n'){
-            return;
-        } 
-         std::string msg = message_pool_[client_fd];
-    
-         std::string reply = execute_command(db_, msg, true) + "\n";
+        //split buffer at \n
+        message_pool_[client_fd].append(buffer,bytes_read);
+        std::vector<std::string> commands = split_keep_newline(message_pool_[client_fd]);
+        message_pool_[client_fd].clear();
+        for(int i = 0; i<commands.size();i++){
+            if(i==commands.size()-1){
+                if(commands[i][commands[i].size()-1]!='\n'){
+                    message_pool_[client_fd].append(commands[i]);
+                    return;
+                }
+            }
+            std::string reply = execute_command(db_, commands[i], true) + "\n";
 
-        ssize_t bytes_sent = send(client_fd, reply.c_str(), reply.length(), 0);
-        if (bytes_sent < 0) {
-            close(client_fd);
+            ssize_t bytes_sent = send(client_fd, reply.c_str(), reply.length(), 0);
+            if (bytes_sent < 0) {
+                message_pool_.erase(client_fd);
+                close(client_fd);
+                return;
+            }
         }
+
+   
+    
+         
         message_pool_[client_fd].clear();      
     }
 } 
